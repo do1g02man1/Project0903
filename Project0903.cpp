3aacda447e22aec74c38fbb2498f22ed6489cb22
dogman0211 <dogman0211@gmail.com>
2025-09-03 17:03:18 +0900

    매직넘버 제거, 코드 최적화

--- Project0903.cpp
+++ Project0903.cpp
@@ -13,7 +13,7 @@ int main()
 
     //printf("원의 넓이는 %.2f입니다.", Radius * Radius * PI);
 
-    //return 0;
+
 
     /* // 1. 숫자를 입력받아 양수인지 음수인지 0인지 출력하기
     int Number;
@@ -49,17 +49,15 @@ int main()
     */
     
     /* // 3. 두 수를 입력 받아 더 큰 수를 출력하기.같을 경우는 같다고 출력.
-    int Number1, Number2, Result;
+    int Number1, Number2;
 
     printf("두 수를 입력해 주세요. ");
     std::cin >> Number1 >> Number2;
 
-    Result = Number1 - Number2;
-
-    if (Result > 0) {
+    if (Number1 > Number2) {
         printf("Number1보다 Number2가 더 큰 숫자입니다.");
     }
-    else if (Result < 0) {
+    else if (Number1 < Number2) {
         printf("Number2보다 Number1가 더 큰 숫자입니다.");
     }
     else {
@@ -68,12 +66,15 @@ int main()
     */
 
     /* // 4. 나이와 키를 입력 받아, 6세 이상, 120cm 이상일 때 롤러코스터 탑승 가능, 그 외에는 불가능으로 출력
-    int Age, Height;
+    const unsigned int LimitAge = 6;
+    const float LimitHeight = 120.0f;
+    unsigned int Age = 0;
+    unsigned int Height = 0;
 
     printf("나이와 키를 입력해 주세요. ");
     std::cin >> Age >> Height;
 
-    if (Age >= 6 && Height >= 120) {
+    if (Age >= LimitAge && Height >= LimitHeight) {
         printf("롤러코스터에 오신걸 환영합니다.");
     }
     else {
@@ -82,21 +83,25 @@ int main()
     */
 
     /* // 5. 점수를 입력 받아 90점 이상은 A, 80점 이상은 B, 70점 이상은 C, 60점 이상은 D, 그 이하는 F라고 출력하기
-    int Grade;
-    
     printf("성적을 입력해 주세요. ");
     std::cin >> Grade; 
     
-    if (Grade >= 90) {
+    int Grade;
+    const int RankA = 90;
+    const int RankB = 80;
+    const int RankC = 70;
+    const int RankD = 60;
+
+    if (Grade >= RankA) {
         printf("A");
     }
-    else if (Grade >= 80) {
+    else if (Grade >= RankB) {
         printf("B");
     }
-    else if (Grade >= 70) {
+    else if (Grade >= RankC) {
         printf("C");
     }
-    else if (Grade >= 60) {
+    else if (Grade >= RankD) {
         printf("D");
     }
     else {
@@ -107,14 +112,16 @@ int main()
     /* // 6. 세 과목의 점수를 입력받아, 세 과목 평균 60점 이상이면 "합격", 아니면 "불합격"을 출력하는 프로그램을 작성하세요.     
        //    한과목이라도 40점 미만이면 불합격 출력하기
     int Kor, Eng, Math;
-    float Avg;
+
+    const int AverageLimit = 60;
+    const int FailLimit = 40;  
+    const float SubmitCount = 3.f;
 
     printf("국어, 영어, 수학의 성적을 입력해 주세요. ");
     std::cin >> Kor >> Eng >> Math;
 
-    Avg = (Kor + Eng + Math) / 3.f;
-
-    if ((Kor >= 40 && Eng >= 40 && Math >= 40) && Avg >= 60) {
+    float Avg = (Kor + Eng + Math) / SubmitCount;
+    if (Avg >= AverageLimit && (Kor >= FailLimit && Eng >= FailLimit && Math >= FailLimit)) {
         printf("합격");
     }
     else {
